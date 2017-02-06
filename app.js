478c119fe05bd6141bbf6068d0ecb6f080620155
fr440305 <fr440305@gmail.com>
2017-02-06 12:10:51 -0800

    Update app.js / Add App.Const(), App.stat[], App.looper()

--- app.js
+++ app.js
@@ -8,5 +8,16 @@ function eventer () {
 }
 
 function App () {
+	this.stat = [this.Const('stat-list')[0], 0];
+}
+
+App.prototype.Const = function (const_name) {
+	return {
+		"stat-list": ["Input_Nickname", "Check_Nickname", "Isolator", "Be_Invited", "Go_Inviting", "Chatting"]
+	}[const_name];
+}
+
+App.prototype.looper = function () {
+
 }
 

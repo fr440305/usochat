7ee1f78a88c8b88c9cf347bd7211ca53c0da4add
fr440305 <fr440305@gmail.com>
2017-02-07 20:02:20 -0800

    Update uso.git / fix bug which you can type any address to access index.html

--- uso.go
+++ uso.go
@@ -8,24 +8,48 @@ import (
 )
 
 func MapFile(file_name string) func(http.ResponseWriter, *http.Request) {
-	return (func(res http.ResponseWriter, req *http.Request) {
-		/* use Library::io to get the address of file */
-		file_content, err := ioutil.ReadFile(file_name)
-		if err != nil {
-			/* Write 404 into ResponseWriter */
-			res.WriteHeader(http.StatusNotFound)
-			return
-		}
-		/* Write file_content into ResponseWrite */
-		res.WriteHeader(http.StatusOK)
-		io.WriteString(res, string(file_content))
-		/* for testing */
-		fmt.Println("Route:", file_name, "--> ", req.URL.Path)
-	})
+	if file_name == "" {
+		return (func(res http.ResponseWriter, req *http.Request) {
+			if req.URL.Path == "/" {
+				fmt.Println("right") /* for index.html */
+				/* use Library::io to get the address of file */
+				file_content, err := ioutil.ReadFile("index.html")
+				if err != nil {
+					/* Write 404 into ResponseWriter */
+					res.WriteHeader(http.StatusNotFound)
+					return
+				}
+				/* Write file_content into ResponseWrite */
+				res.WriteHeader(http.StatusOK)
+				io.WriteString(res, string(file_content))
+				/* for testing */
+				fmt.Println("Route: index.html --> ", req.URL.Path)
+			} else {
+				fmt.Println("wrong") /* 404 */
+				res.WriteHeader(http.StatusNotFound)
+				return
+			}
+		})
+	} else {
+		return (func(res http.ResponseWriter, req *http.Request) {
+			/* use Library::io to get the address of file */
+			file_content, err := ioutil.ReadFile(file_name)
+			if err != nil {
+				/* Write 404 into ResponseWriter */
+				res.WriteHeader(http.StatusNotFound)
+				return
+			}
+			/* Write file_content into ResponseWrite */
+			res.WriteHeader(http.StatusOK)
+			io.WriteString(res, string(file_content))
+			/* for testing */
+			fmt.Println("Route:", file_name, "--> ", req.URL.Path)
+		})
+	}
 }
 
 func main() {
-	http.HandleFunc("/", MapFile("index.html"))
+	http.HandleFunc("/", MapFile(""))
 	http.HandleFunc("/index.html", MapFile("index.html"))
 	http.HandleFunc("/app.js", MapFile("app.js"))
 	http.HandleFunc("/api.js", MapFile("api.js"))

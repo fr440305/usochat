6ef46ef27b3b2df33594e829f6249e3b46598248
fr440305 <fr440305@gmail.com>
2017-02-08 01:05:26 -0800

    Update api.js / Toughly complete Client.Get() and Post().

--- api.js
+++ api.js
@@ -13,6 +13,8 @@ function theard() {
 function client() {
 	this.id = undefined;
 	this.ajax = new XMLHttpRequest();
+	this.get_url = "get";
+	this.post_url = "post";
 }
 
 client.prototype.AddEventListener = function(event_name, callback) {
@@ -22,10 +24,17 @@ client.prototype.AddEventListener = function(event_name, callback) {
 	}
 }
 
-client.prototype.Post = function (url, post_content) {
+client.prototype.Post = function (post_content) {
+	/* post_content : string = \" post_key \= post_value [{ \& post_key \= post_value }] \" */
+	this.ajax.open("POST", this.post_url, true);
+	this.ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	this.ajax.send(post_content);
 }
 
-client.prototype.Get = function (url) {
+client.prototype.Get = function (get_req_content) {
+	/* get_req_content : string = \" get_req_key \= get_req_value [{ \& get_req_key \= get_req_value }] \" */
+	this.ajax.open("GET", this.get_url + '?' + get_req_content, true);
+	this.ajax.send();
 }
 
 

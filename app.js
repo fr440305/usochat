acfbf161b57deae8f9d99f1058a687d2a4ed1c70
fr440305 <fr440305@gmail.com>
2017-02-13 22:45:36 -0800

    app.js / Add class::app()

--- app.js
+++ app.js
@@ -5,6 +5,21 @@
 /* import api.js in var::$api */
 var $api = new API();
 
+function app () {
+	this.UI = new ui();
+	this.req_interval = 250;
+	var Me = this;
+	setInterval(function(){
+		var dia_arr = $api.Client.Var("dialogs");
+		if (dia_arr.length !== Me.UI.Var("dia-node-length") ) {
+			Me.UI.ClearDiaNodes();
+			for (var i = 0; i < dia_arr.length; i++) {
+				Me.UI.AddDiaNode(dia_arr[i]);
+			}
+		}
+	}, Me.req_interval, true);
+}
+
 function ui () {
 	this.main = document.getElementById("ui");
 	this.dia_board = document.getElementById("-uso-");
@@ -22,19 +37,8 @@ ui.prototype.ClearDiaNodes = function () {
 
 ui.prototype.Var = function (key) {
 	return {
-		"dia-node-length": this.dia_borad.children.length
+		"dia-node-length": this.dia_board.childElementCount
 	}[key];
 }
 
-var $ui = new ui();
-
-setInterval(function(){
-	$ui.ClearDiaNodes();
-	var dia_arr = $api.Client.Var("dialogs");
-	for (var i = 0; i < dia_arr.length; i++) {
-		$ui.AddDiaNode(dia_arr[i]);
-	}
-	//document.getElementById('-uso-').innerHTML = $api.Client.Var("dialogs");
-	//console.log($api.Client.dialogs);
-}, 1000, true);
-
+var $app = new app();

055ac599a1caacb1c7b10f1afdb34857a2908502
fr440305 <fr440305@gmail.com>
2017-02-13 17:38:47 -0800

    app.js / Add ui class.

--- app.js
+++ app.js
@@ -5,8 +5,36 @@
 /* import api.js in var::$api */
 var $api = new API();
 
+function ui () {
+	this.main = document.getElementById("ui");
+	this.dia_board = document.getElementById("-uso-");
+}
+
+ui.prototype.AddDiaNode = function (dia_content) {
+	var new_dia_node = document.createElement("p");
+	new_dia_node.innerHTML = dia_content;
+	this.dia_board.appendChild(new_dia_node);
+}
+
+ui.prototype.ClearDiaNodes = function () {
+	this.dia_board.innerHTML = ''; /* not the best option. Need to be refactored */
+}
+
+ui.prototype.Var = function (key) {
+	return {
+		"dia-node-length": this.dia_borad.children.length
+	}[key];
+}
+
+var $ui = new ui();
+
 setInterval(function(){
-	document.getElementById('-uso-').innerHTML = $api.Client.Var("dialogs");
+	$ui.ClearDiaNodes();
+	var dia_arr = $api.Client.Var("dialogs");
+	for (var i = 0; i < dia_arr.length; i++) {
+		$ui.AddDiaNode(dia_arr[i]);
+	}
+	//document.getElementById('-uso-').innerHTML = $api.Client.Var("dialogs");
 	//console.log($api.Client.dialogs);
 }, 1000, true);
 

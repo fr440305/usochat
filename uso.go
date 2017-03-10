a02f0eefe1a0264bcbf1178bb523b9391e2cb3d9
fr440305 <fr440305@gmail.com>
2017-03-09 22:48:48 -0800

    server & api - try to use multiport to handle the req, but failed.

--- uso.go
+++ uso.go
@@ -3,56 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"log"
+	//"io"
+	//"io/ioutil"
 	"net/http"
 	"strings"
 )
 
 var dialogs []string
 
-func MapFile(file_name string) func(http.ResponseWriter, *http.Request) {
-	if file_name == "" {
-		return (func(res http.ResponseWriter, req *http.Request) {
-			if req.URL.Path == "/" {
-				fmt.Println("right") /* for index.html */
-				/* use Library::io to get the address of file */
-				file_content, err := ioutil.ReadFile("index.html")
-				if err != nil {
-					/* Write 404 into ResponseWriter */
-					res.WriteHeader(http.StatusNotFound)
-					return
-				}
-				/* Write file_content into ResponseWrite */
-				res.WriteHeader(http.StatusOK)
-				io.WriteString(res, string(file_content))
-				/* for testing */
-				fmt.Println("Route: index.html --> ", req.URL.Path)
-			} else {
-				fmt.Println("wrong") /* 404 */
-				res.WriteHeader(http.StatusNotFound)
-				return
-			}
-		})
-	} else {
-		return (func(res http.ResponseWriter, req *http.Request) {
-			/* use Library::io to get the address of file */
-			file_content, err := ioutil.ReadFile(file_name)
-			if err != nil {
-				/* Write 404 into ResponseWriter */
-				res.WriteHeader(http.StatusNotFound)
-				return
-			}
-			/* Write file_content into ResponseWrite */
-			res.WriteHeader(http.StatusOK)
-			io.WriteString(res, string(file_content))
-			/* for testing */
-			fmt.Println("Route:", file_name, "--> ", req.URL.Path)
-		})
-	}
+type GetHandler struct {
 }
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
+func (GH GetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	/* for testing */
 	fmt.Println("<!--get")
@@ -79,7 +42,10 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("-->")
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
+type PostHandler struct {
+}
+
+func (PH PostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	/* for testing */
 	fmt.Println("<!--post")
@@ -99,12 +65,15 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	/* File Server */
-	http.HandleFunc("/", MapFile(""))
-	http.HandleFunc("/index.html", MapFile("index.html"))
-	http.HandleFunc("/app.js", MapFile("app.js"))
-	http.HandleFunc("/api.js", MapFile("api.js"))
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/post", PostHandler)
-	http.ListenAndServe(":8080", nil)
+	//http.HandleFunc("/", MapFile(""))
+	//http.HandleFunc("/index.html", MapFile("index.html"))
+	//http.HandleFunc("/app.js", MapFile("app.js"))
+	//http.HandleFunc("/api.js", MapFile("api.js"))
+	//http.HandleFunc("/get", GetHandler)
+	//http.HandleFunc("/post", PostHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	log.Fatal(http.ListenAndServe(":8081", new(GetHandler)))
+	log.Fatal(http.ListenAndServe(":8082", new(PostHandler)))
 	fmt.Println("vim-go")
 }

6787ccf825db2ac0704834358f5a4515e5463fd3
fr440305 <fr440305@gmail.com>
2017-03-11 11:37:14 -0800

    server - done the basic webserver Server

--- server.main.go
+++ server.main.go
@@ -6,11 +6,33 @@ import (
 	"log"
 	//"io"
 	//"io/ioutil"
+	"github.com/gorilla/websocket"
 	"net/http"
 	"strings"
 )
 
 var dialogs []string
+
+func WebSocketServFunc(w http.ResponseWriter, r *http.Request) {
+	musubi, err := websocket.Upgrader(w, r, nil)
+	if err != nil {
+		log.Print("Upgrade Err: ", err)
+		return
+	}
+	defer musubi.Close()
+	for {
+		msg_type, msg_cx, err := musubi.ReadMessage()
+		if err != nil {
+			log.Print("Read Msg Err: ", err)
+			break
+		}
+		err = musubi.WriteMessage(msg_type, msg_cx)
+		if err != nil {
+			log.Print("Write Msg Err: ", err)
+			break
+		}
+	}
+}
 
 func main() {
 	/* File Server */
@@ -18,8 +40,7 @@ func main() {
 	http.HandleFunc("/index.html", ServeFile("index.html"))
 	http.HandleFunc("/app.js", ServeFile("app.js"))
 	http.HandleFunc("/api.js", ServeFile("api.js"))
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/post", PostHandler)
+	http.HandleFunc("/ws", WebSocketServFunc)
 
 	log.Fatal(http.ListenAndServe(":8888", nil))
 	fmt.Println("vim-go")

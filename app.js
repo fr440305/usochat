2e859234020efc9f368d074bd289f7ea95ff73dd
fr440305 <fr440305@gmail.com>
2017-03-11 12:25:42 -0800

    app

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ function Eventer () {
 
 function Conner () {
 	this.ws = new WebSocket("ws://" + document.location.host + "/ws");
+	this.wsInit();
+}
+
+Conner.prototype.wsInit = function () {
 }
 
 

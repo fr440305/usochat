09ada136a3b091f64cbf38d0b3bee9e3afc6f292
fr440305 <fr440305@gmail.com>
2017-03-11 11:24:23 -0800

    server - remove the old ajax server; index - remove the js-tag. I wanna rewrite the whole prj by using the websocket.

--- server.main.go
+++ server.main.go
@@ -12,63 +12,6 @@ import (
 
 var dialogs []string
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	/* for testing */
-	fmt.Println("<!--get")
-	fmt.Println("  Method = ", r.Method)
-	fmt.Println("  RawQuery = ", r.URL.RawQuery)
-	for d, r := range r.Form {
-		fmt.Println("  Form[", d, "] |-> ", r)
-		if d == "conversation" {
-			fmt.Println("  conversation:")
-			/* TODO - pretend XSS attack, pay attention to security */
-			resp_json, err := json.Marshal(dialogs)
-			if err != nil {
-				fmt.Println("http-get-Fatal!!-json.Marshal")
-				return
-			}
-			w.Header().Set("Content-Type", "text/json")
-			w.WriteHeader(http.StatusOK)
-			w.Write(resp_json)
-			for index, diaelm := range dialogs {
-				fmt.Println("    dialog[", index, "] = ", diaelm)
-			}
-		}
-	}
-	fmt.Println("-->")
-}
-
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	/* for testing */
-	fmt.Println("<!--post")
-	if r.Method == "POST" {
-		fmt.Println("  Method = ", r.Method, ", normal")
-	} else {
-		fmt.Println("  Method = ", r.Method, ", ALERT!")
-	}
-	for k, v := range r.PostForm {
-		fmt.Println("  PostForm[", k, "] |-> ", v)
-		if k == "dialog" {
-			dialogs = append(dialogs, strings.Join(v, ""))
-		}
-	}
-	fmt.Println("-->")
-}
-
-func ServeHome() func(http.ResponseWriter, *http.Request) {
-	return (func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-}
-
-func ServeFile(filename string) func(http.ResponseWriter, *http.Request) {
-	return (func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filename)
-	})
-}
-
 func main() {
 	/* File Server */
 	http.HandleFunc("/", ServeHome())

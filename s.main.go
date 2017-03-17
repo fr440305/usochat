0e2c0fb4ae45407d76c50f17bc3272acf76075ca
fr440305 <fr440305@gmail.com>
2017-03-17 01:15:20 -0700

    index s.main - Add a basic webpage and a file-server

--- s.main.go
+++ s.main.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+import "net/http"
+import "github.com/gorilla/websocket"
+
+func main() {
+	fmt.Println("http://127.0.0.1:9999")
+
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("/ws - accessing....")
+	})
+	http.ListenAndServe(":9999", nil)
+}

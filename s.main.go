f18bf09f076b83c37302c86b4f38b7f3dd3cd07b
fr440305 <fr440305@gmail.com>
2017-03-18 01:05:37 -0700

    s.main - combine the serving code to a new func, add some comment, find a bug.

--- s.main.go
+++ s.main.go
@@ -1,3 +1,5 @@
+//BUG - After the client exit, the number in center will not reduce.
+//TODO - Add a onclose event in frontend.
 package main
 
 import "fmt"
@@ -13,12 +15,43 @@ func newCenter() *Center {
 	return new(Center)
 }
 
+//return the number of people online:
 func (c *Center) GetOnliner() []byte {
+	//convert c.num_online, which is int, to a byte array:
 	return []byte(strconv.Itoa(c.num_onliner))
 }
 
 func (c *Center) AddOnliner() {
 	c.num_onliner += 1
+}
+
+func ServeWs(upgrader websocket.Upgrader, center *Center, w http.ResponseWriter, r *http.Request) {
+	//Initialization:
+	//create the connection:
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	fmt.Println("/ws - accessing....")
+	center.AddOnliner()
+	//Polling:
+	for {
+		//Read message from the client:
+		//code will be blocked here until it received msg:
+		msg_type, msg_cx, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Fatal - conn--readmsg")
+			return
+		}
+		fmt.Println(msg_type, msg_cx)
+		//Write the messages(how many people onlines) to the client:
+		err = conn.WriteMessage(websocket.TextMessage, center.GetOnliner())
+		if err != nil {
+			fmt.Println("Fatal - conn--response")
+			return
+		}
+		fmt.Println("conn--response....!")
+	}
 }
 
 func main() {
@@ -28,33 +61,11 @@ func main() {
 		WriteBufferSize: 1024,
 	}
 	var center = newCenter()
+	//To serve the webpages to the client:
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	//To handle the websocket request:
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		//create the connection:
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			return
-		}
-		fmt.Println("/ws - accessing....")
-		center.AddOnliner()
-		//Polling:
-		for {
-			//Read message from the client:
-			//code will be blocked here until it received msg:
-			msg_type, msg_cx, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println("Fatal - conn-readmsg")
-				return
-			}
-			fmt.Println(msg_type, msg_cx)
-			//Write the messages to the client:
-			err = conn.WriteMessage(websocket.TextMessage, center.GetOnliner())
-			if err != nil {
-				fmt.Println("Fatal - conn--response")
-				return
-			}
-			fmt.Println("conn--response....!")
-		}
+		ServeWs(upgrader, center, w, r)
 	})
 	http.ListenAndServe(":9999", nil)
 }

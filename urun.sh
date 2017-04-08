06df0cb15367818800174656f660df3ca3381691
fr440305 <fr440305@gmail.com>
2017-04-08 16:16:58 -0700

    dev.urun - add some comments.

--- urun.sh
+++ urun.sh
@@ -1,2 +1,15 @@
+#urun.sh
+#You can use this bash file if you wanna run it safely.
+
+#Usage:
+#
+#	"urun" <command> <flags>.
+#
+
+
+echo "$BASH , $BASH_ARGC , ${BASH_ARGV[0]}, ${BASH_ARGV[1]}"
+
 go build -o ./uso.out ./*.go
-( ./uso.out 1>./log/std.log 2>./log/err.log )
+#( ./uso.out 1>./log/std.log 2>./log/err.log )
+
+

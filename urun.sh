168a95fd46b0171d49c1f01b64a873855033e24e
fr440305 <fr440305@gmail.com>
2017-04-09 15:50:01 -0700

    dev.urun - add some argments handler.

--- urun.sh
+++ urun.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #urun.sh
 #You can use this bash file if you wanna run it safely.
 
@@ -7,9 +9,54 @@
 #
 
 
-echo "$BASH , $BASH_ARGC , ${BASH_ARGV[0]}, ${BASH_ARGV[1]}"
+printHelp () {
+	echo "";
+	echo "USO is a simple chatting website, and this file,";
+	echo "urun.sh, is the booter of this website\'s server.";
+	echo "";
+	echo "Usage:";
+	echo "        [./]urun.sh <command> <flags>";
+	echo "";
+	echo "If you wanna run it, type \`urun.sh start\`.";
+	echo "";
+};
 
-go build -o ./uso.out ./*.go
-#( ./uso.out 1>./log/std.log 2>./log/err.log )
+build () {
+	go build -o ./uso.out ./*.go;
+};
 
+#The following function runs the uso.out.
+run () {
+	read run_mode;
+	echo $run_mode;
+	if [[ $run_mode == quite ]]; then {
+		echo $run_mode;
+		echo "--quite";
+		./uso.out 1>./u.std.log 2>./u.err.log;
+	}; elif [[ $run_mode == noise ]]; then {
+		echo "--noise";
+		./uso.out;
+	}; fi;
+};
 
+#The following function extracts the arguments of this bash,
+#analize it, and ...
+parseArgs () {
+	if [[ $BASH_ARGC -eq 0 ]]; then {
+		#$ urun.sh;
+		printHelp;
+	}; else {
+		build;
+		command=${BASH_ARGV[$(($BASH_ARGC-1))]};
+		echo The command is $command;
+		if [[ $command == start ]]; then {
+			{ echo noise; }|run;
+		}; elif [[ $command == qstart ]]; then {
+			{ echo quite; }|run;
+		}; else {
+			echo $command is an invalid command!;
+		};fi;
+	}; fi;
+};
+
+parseArgs;

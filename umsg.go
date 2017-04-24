1e44f4e3b0a1d21f72352a2af17ee384b52b35ca
fr440305 <fr440305@gmail.com>
2017-04-24 15:12:42 -0700

    room.umsg - modified the structure of Msg.

--- umsg.go
+++ umsg.go
@@ -10,10 +10,11 @@ const SET_DESCRIPTION byte = 1
 const SET_CONTENT byte = 2
 
 type Msg struct {
-	usor        *Usor
-	room        *Room
-	description string
-	content     []string
+	usor    *Usor
+	room    *Room
+	meta    *Msg
+	summary string
+	content []string
 }
 
 func newMsg(source_usor *Node) *Msg {
@@ -26,45 +27,11 @@ func (M *Msg) clone() *Msg {
 func (M *Msg) set(set_what byte, description string, content []string) *Msg {
 }
 
-//Pay attention to the probobaly-appear errors.
 func (M *Msg) parseJSON(json_raw string) error {
-	var user_msg struct {
-		SouceNode   string   `json:"source_node"`
-		Description string   `json:"description"`
-		Content     []string `json:"content"`
-	}
-	json.Unmarshal([]byte(json_raw), &user_msg)
-	M.set(SET_DESCRIPTION|SET_CONTENT, user_msg.Description, user_msg.Content)
-	return nil
 }
 
-//TODO - check error
-//This method transforms the Msg::M to JSON string.
 func (M *Msg) toJSON() string {
-	var res []byte
-	var err error
-	var source_node_iden string
-	if M.source_node == nil {
-		source_node_iden = ""
-	} else {
-		source_node_iden = M.source_node.idString()
-	}
-	var user_msg = struct {
-		SouceNode   string   `json:"source_node"`
-		Description string   `json:"description"`
-		Content     []string `json:"content"`
-	}{source_node_iden, M.description, M.content}
-	res, err = json.Marshal(user_msg)
-	if err != nil {
-		//TODO - error handler goes here...
-	}
-	return string(res)
-	//return `{"content":["toJSON","toJSON"]}`
 }
 
 func (M Msg) Error() string {
-	if M.description == "error" && M.content != nil && len(M.content) != 0 {
-		return M.content[0]
-	}
-	return ""
 }

d6a82c68da19d8aeff318640e0fce3a309dde2ea
fr440305 <fr440305@gmail.com>
2017-04-25 18:12:32 -0700

    room.unode - redesign the Node type.

--- unode.go
+++ unode.go
@@ -14,11 +14,7 @@ type Usor struct {
 	msgchan chan Msg
 	room    *Room
 	conn    *websocket.Conn //connent client to node
-	nid     int64           //node id
-}
-
-func newUsor(upgdr websocket.Upgrader, w http.ResponseWriter, r *http.Request) *Usor {
-	return nil
+	nid     uint64          //node id
 }
 
 //eg - ("id")-->(0, "0");
@@ -35,12 +31,8 @@ type Room struct {
 	name      string
 	msg_queue chan Msg
 	msg_hist  []Msg
-	members   []Usor
+	members   []*Usor //points to usors in Center.usorpool
 	center    *Center
-}
-
-func newRoom() *Room {
-	return nil
 }
 
 func (R *Room) rmUsor(rm_usr *Usor) error {
@@ -66,7 +58,8 @@ func (R *Room) push(m Msg) error {
 type Center struct {
 	pid         int //process id
 	msg_q       chan Msg
-	rooms       []Room
+	rooms       []*Room
+	usors       []*Usor
 	ws_upgrader websocket.Upgrader //const
 }
 
@@ -74,19 +67,38 @@ func newCenter(pid int) *Center {
 	var center = new(Center)
 	center.pid = pid
 	center.msg_q = make(chan Msg)
-	center.rooms = []Room{}
+	center.newRoom("Eden")
+	_ulog("@pid@", pid)
 	return center
 }
 
-func (C *Center) handleRooms() {
+func (C Center) validRoomId() uint64 {
+}
+
+func (C Center) validUsorId() uint64 {
+}
+
+func (C *Center) newRoom(name string) *Room {
+	return nil
+}
+
+func (C *Center) newUsor(room *Room, w http.ResponseWriter, r *http.Request) *Usor {
+
+}
+
+//return that how many time it sent.
+func (C *Center) boardcast() int {
+}
+
+func (C *Center) handleRooms() error {
 }
 
 func (C *Center) run() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_ulog("Center.run()", "ennn...")
-	})
+	http.Handle("/", http.FileServer(http.Dir("frontend")))
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		//C.rooms[0].newUsor(C.validUsorId()).run()
+		_ulog("Center.run()", "/ws")
+		C.newUsor(C.rooms[0], w, r).run()
 	})
-	http.ListenAndServe(":9999", nil)
+	go http.ListenAndServe(":9999", nil)
+	C.handleRooms()
 }

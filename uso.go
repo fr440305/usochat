530807541ca372eed472571c3aa7f73a33d6a048
fr440305 <fr440305@gmail.com>
2017-08-06 20:48:44 -0700

    ...

--- uso.go
+++ uso.go
@@ -8,19 +8,33 @@ import "fmt"
 
 // MsgType:
 const (
-	UsoMsg_ToRoom   = "um:inr"
+	//["um:inr", <room name>]
+	UsoMsg_ToRoom = "um:inr"
+
+	//["um:++r", <room name>]
 	UsoMsg_AddRoom  = "um:++r"
 	UsoMsg_ExitRoom = "um:exr"
 	UsoMsg_Say      = "um:say"
-	UsoMsg_Die      = "um:die"
 
-	HallHorn_UsoToHall   = "hh:uinh"
+	// ["um:die"]
+	UsoMsg_Die = "um:die"
+
+	// ["hh:uinh", <len of connpool>]
+	HallHorn_UsoToHall = "hh:uinh"
+
+	// ["hh:uexh", <len of connpool>]
 	HallHorn_UsoExitHall = "hh:uexh"
-	HallHorn_UsoAddRoom  = "hh:u++r"
-	HallHorn_DelRoom     = "hh:r--"
-	HallHorn_Error       = "hh:err"
 
+	// ["hh:u++r", <room name>]
+	HallHorn_UsoAddRoom = "hh:u++r"
+
+	// ["hh:r--", <room 1>, <room 2>, ..., <room n>]
+	HallHorn_DelRoom = "hh:r--"
+
+	// ["hr:rooms", <room 1>, <room 2>, ... , <room n>]
 	HallResp_Rooms = "hr:rooms"
+
+	// ["hr:err", <error description>]
 	HallResp_Error = "hr:err"
 )
 
@@ -66,7 +80,7 @@ func (c *Conn) Read() (string, []string) {
 	}
 	msg := NewMessage(bar)
 	if len(msg) == 0 {
-		return "", nil
+		return "", []string{}
 	} else {
 		return msg[0], msg[1:]
 	}
@@ -88,14 +102,14 @@ func (h Hall) horn(msg_type string, msg_cont ...string) {
 
 func (h *Hall) addAndHorn(uc *Conn) {
 	*h = append(*h, uc)
-	h.horn(HallHorn_UsoToHall)
+	h.horn(HallHorn_UsoToHall, connpool_lentostr())
 }
 
 func (h *Hall) delAndHorn(uc *Conn) {
 	for i := 0; i < len(*h); i++ {
 		if (*h)[i] == uc {
 			*h = append((*h)[:i], (*h)[i+1:]...)
-			h.horn(HallHorn_UsoExitHall)
+			h.horn(HallHorn_UsoExitHall, connpool_lentostr())
 			return
 		}
 	}
@@ -104,18 +118,44 @@ func (h *Hall) delAndHorn(uc *Conn) {
 func (h *Hall) ServeGuest(uc *Conn) {
 	h.addAndHorn(uc)
 	fmt.Println(h)
-	uc.Write(HallResp_Rooms, "waiting..")
+	uc.Write(HallResp_Rooms, roompool_namelist()...)
 	for {
 		ty, co := uc.Read()
-		if ty == UsoMsg_Die {
+		fmt.Println("Hall.ServeGuest ty, co == ", ty, co)
+		switch ty {
+		case UsoMsg_Die:
 			fmt.Println("Hall.ServeGuest die")
 			h.delAndHorn(uc)
 			return
-		}
-		fmt.Println("Hall.ServeGuest ty, co == ", ty, co)
-		switch ty {
 		case UsoMsg_ToRoom:
+			if len(co) == 0 {
+				uc.Write(HallResp_Error, "Room name is required.")
+				return
+			}
+			room := roompool_getRoomByName(co[0])
+			if room == nil {
+				uc.Write(HallResp_Error, "No such room.")
+				return
+			}
+			h.delAndHorn(uc)
+			room.ServeMember(uc)
+			if uc.Quit {
+				return
+			} else {
+				h.addAndHorn(uc)
+				uc.Write(HallResp_Rooms, roompool_namelist()...)
+			}
 		case UsoMsg_AddRoom:
+			if len(co) == 0 {
+				uc.Write(HallResp_Error, "Room name is required.")
+				return
+			}
+			room := roompool_add(co[0])
+			if room == nil {
+				uc.Write(HallResp_Error, "Cannot add this room.")
+			} else {
+				h.horn(HallHorn_UsoAddRoom, co[0])
+			}
 		default:
 			uc.Write(HallResp_Error, "Invalid message")
 		}
@@ -182,18 +222,44 @@ func connpool_lentostr() string {
 var Uso_roompool = []*Room{} // -> var uso_RoomPool = RoomPool{}
 
 func roompool_add(name string) *Room {
-	return nil
+	if name == "" || roompool_getRoomByName(name) != nil {
+		return nil
+	}
+	room := &Room{
+		Name:  name,
+		Hist:  []string{},
+		Conns: []*Conn{},
+	}
+	Uso_roompool = append(Uso_roompool, room)
+	return room
 }
 
-func roompool_del(r *Room) {
+func roompool_del(room *Room) {
+	for i, r := range Uso_roompool {
+		if r == room {
+			Uso_roompool = append(
+				Uso_roompool[:i],
+				Uso_roompool[i+1:]...,
+			)
+		}
+	}
 }
 
 func roompool_getRoomByName(name string) *Room {
+	for _, r := range Uso_roompool {
+		if r.Name == name {
+			return r
+		}
+	}
 	return nil
 }
 
 func roompool_namelist() []string {
-	return nil
+	list := []string{}
+	for _, r := range Uso_roompool {
+		list = append(list, r.Name)
+	}
+	return list
 }
 
 // export
